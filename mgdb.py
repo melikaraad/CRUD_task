690967db737d1ecd6d4b8ec0e6eab2140ebb110e
melikaraad <124432068+melikaraad@users.noreply.github.com>
2023-08-16 15:28:33 +0330

    mgdb.py

--- mgdb.py
+++ mgdb.py
@@ -2,5 +2,5 @@ from pymongo import MongoClient
 from model import ToDo
 
 client = MongoClient()
-db = client['toDo', ToDo]
-collection = db['toDos', ToDo]
+db = client['toDo']
+collection = db['toDos']
