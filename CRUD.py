603b5467bc9609c7bf0891018138795ca0bbe964
melikaraad <124432068+melikaraad@users.noreply.github.com>
2023-08-19 14:06:27 +0330

    CRUD.py

--- CRUD.py
+++ CRUD.py
@@ -2,29 +2,23 @@ from fastapi import FastAPI
 from model import ToDo
 from mgdb import collection
 
-app_get = FastAPI()
+app = FastAPI()
 
-@app_get.get("/toDos")
+@app.get("/toDos")
 def get_todos():
     return collection.find()
 
-app_post = FastAPI()
-
-@app_post.post("/toDos")
+@app.post("/toDos")
 def create_todo(toDo: ToDo):
     collection.insert_one(toDo.dict())
     return toDo
 
-app_put = FastAPI()
-
-@app_put.put("/toDos/{id}")
+@app.put("/toDos/{id}")
 def update_todo(id: int, toDo: ToDo):
     collection.update_one({'id': id}, {'$set': toDo.dict()})
     return toDo
-
-app_delete = FastAPI()
  
-@app_delete.delete("/toDos/{id}")
+@app.delete("/toDos/{id}")
 def delete_todo(id: int):
     collection.delete_one({'id': id})
     return {'message': 'Todo deleted'}
