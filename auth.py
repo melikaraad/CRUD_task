09adf941ead2aab861ec4c17f72cc7741924fc70
melikaraad <124432068+melikaraad@users.noreply.github.com>
2023-08-27 17:34:04 +0330

    auth.py

--- auth.py
+++ auth.py
@@ -12,6 +12,12 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
+class User(BaseModel):
+    username: str
+    password: str 
+
+registered_users = []
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_user(token: str = Depends(oauth2_scheme)):
@@ -29,9 +35,16 @@ def get_user(token: str = Depends(oauth2_scheme)):
         raise credentials_exception
     return username
 
+@app.post("/signup")
+
+async def create_user(user: User):
+        registered_users.append(user) 
+        return {"message": "User created successfully"}
+
 @app.post("/token", response_model=Token)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    if form_data.username == "USERNAME" and form_data.password == "my-PASSWORD":
+    user = next((u for u in registered_users if u.username == form_data.username), None)
+    if user and user.password == form_data.password:
         access_token_expires = timedelta(minutes=30)
         access_token = jwt.encode(
             {"sub": form_data.username, "exp": datetime.utcnow() + access_token_expires},
@@ -45,4 +58,5 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    
 
